779f0ddda275f16ab151c70977b43220615c32e9
Stefan1711UNI <stefan.sonderling1@student.nhlstenden.com>
2025-03-27 21:43:50 +0100

    continued add instance and view instances

--- oop_remake.py
+++ oop_remake.py
@@ -41,11 +41,16 @@ class PasswordManager:
 
     def add_instance(self):
         try:
+            #website id gotten from function
+            website_id = self.find_websiteid_from_str(input("Enter website name: "))
             username = input("Enter username: ")
-            email = input(print("Enter email: "))
-            password = input("Enter password: ")
+            email = input("Enter email: ")
+            password = input("Enter password: ")    #then gets hashed and that gets stored
+            url = input("Enter URL: ")
+            #get salt 
             #check if website exits and add if not
-            db.execute('''INSERT INTO instances (username, email, password) VALUES (?, ?, ?)''', (username, email, password))
+            db.execute('''INSERT INTO Instances (website_id, url, username, email, password_hash, salt) VALUES (?, ?, ?, ?, ?, ?)''', 
+                       (website_id, url, username, email, password, "salt"))
             connect.commit()
             print("Instance added successfully!!!")
         except sqlite3.Error as e:
@@ -54,36 +59,76 @@ class PasswordManager:
         self.navigate_menu()
 
 
+    def find_websiteid_from_str(self, user_input):
+        str_website = user_input.strip()
+        websites = self.get_websites()
+        for website in websites:
+            website_id, website_name = website
+            if str_website.lower() == website_name.lower():
+                return website_id
+    
+        #add website since none exist
+        return self.add_website(str_website)
+            
+        
+    
+
+    def add_website(self, website_name):
+        try:
+            db.execute('''INSERT INTO Websites (name) VALUES (?)''', (website_name,))
+            connect.commit()
+            db.execute('''SELECT id FROM Websites WHERE name = (?) ''', (website_name,))
+            website_id = db.fetchone()
+            print(website_id)
+            if website_id:
+                return website_id[0]
+            else:
+                print("Error: Failed to retrieve new website ID.")
+                return None
+        except sqlite3.Error as e:
+            self.navigate_menu()
+            print(f"Error: Could not add website. {e}")
+            
+
+
     def view_websites(self):
         try:
-            db.execute('''Select * FROM Websites''')
-            websites = db.fetchall()
+            websites = self.get_websites()
             for website in websites:
-                print("")
                 print("-" * 50)
-                print(f"ID: {websites[0]}")
-                print(f"{website[1]}")
+                print(f"{website[0]}: Name: {website[1]}")
                 print("-" * 50)
                 print("")
             #change
+            print("Enter website ID to see it's instances.")
             while True:
-                if input("Enter 'q' to quit: ").lower() == "q":
-                    self.navigate_menu()
+                user_input = int(input("Enter website ID: "))
+                if user_input < 1 or user_input > len(websites):
+                    print("Invalid website ID!")
+                else:
+                    self.view_instances(user_input)
         except sqlite3.Error as e:
             print(f"Error: Could not view websites. {e}")
+        
 
+    def get_websites(self):
+        db.execute('''Select * FROM Websites ORDER BY name ASC''')
+        websites = db.fetchall()
+        lst = []
+        for website in websites:
+            lst.append(website)
+        return lst
 
-    def view_instances(self):
+    def view_instances(self, website_id):
         try:
-            db.execute('''SELECT * FROM instances''')
+            db.execute('''SELECT * FROM Instances WHERE website_id = (?)''', (website_id,))
             instances = db.fetchall()
             for instance in instances:
-                print("")
                 print("-" * 50)
                 print(f"ID: {instance[0]}")
-                print(f"Username: {instance[1]}")
-                print(f"Email: {instance[2]}")
-                print(f"Password: {instance[3]}")
+                print(f"Username: {instance[3]}")
+                print(f"Email: {instance[4]}")
+                print(f"Password: {instance[5]}")
                 print("-" * 50)
                 print("")
             while True:
@@ -101,21 +146,22 @@ class PasswordManager:
 
 
     def navigate_menu(self):
+        print("")
         print("1. Add new instance")
         print("2. View all instances")
-        print("3. Delete instance")
-        print("4. Update instance")
+        print("3. Update instance")
+        print("4. Delete instance")
         print("5. Exit")
         print("")
         choice = input("Enter your choice: ")
         if choice == '1':
-            self.view_websites()
+            self.add_instance()
         elif choice == '2':
-            self.view_instances()
+            self.view_websites()
         # elif choice == '3':
-        #     delete_instance()
-        # elif choice == '4':
         #     update_instance()
+        # elif choice == '4':
+        #     delete_instance()
         elif choice == '5':
             exit()
         else:
